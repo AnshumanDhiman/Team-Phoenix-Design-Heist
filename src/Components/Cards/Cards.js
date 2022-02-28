5369b6de3b0b0fed9114b75aa2d0c8a132ba8300
AnshumanDhiman <67755381+AnshumanDhiman@users.noreply.github.com>
2022-02-28 14:54:00 +0530

    initial commit

---
+++ src/Components/Cards/Cards.js
@@ -0,0 +1,200 @@
+import './Cards.css'
+
+import image from '../../assets/images/img1.png'
+import { Row, Col, Card } from 'antd';
+
+
+
+function Cards(props){
+    return (
+        <>
+        <Row className='row-edit'>
+      <Col span={6} className='col-edit'>   
+    
+       <Card
+    hoverable
+    style={{ width: 250 }}
+    className='card-edit'
+    cover={<img alt="example" src={image} />}
+  >
+  <p className='rating-edit'>4.3 ★ | 40.9K</p>
+  </Card>
+  <br/>
+  <h2>Data is here</h2>
+  <p>Hello I am data</p>
+  <p>Rs. 1499</p>
+  </Col>
+  <Col span={6} className='col-edit'>   
+    
+    <Card
+ hoverable
+ style={{ width: 250 }}
+ className='card-edit'
+ cover={<img alt="example" src={image} />}
+>
+<p className='rating-edit'>4.3 ★ | 40.9K</p>
+</Card>
+<br/>
+<h2>Data is here</h2>
+<p>Hello I am data</p>
+<p>Rs. 1499</p>
+</Col>
+  <Col span={6} className='col-edit'>   
+    
+    <Card
+ hoverable
+ style={{ width: 250 }}
+ className='card-edit'
+ cover={<img alt="example" src={image} />}
+>
+<p className='rating-edit'>4.3 ★ | 40.9K</p>
+</Card>
+<br/>
+<h2>Data is here</h2>
+<p>Hello I am data</p>
+<p>Rs. 1499</p>
+</Col>
+  <Col span={6} className='col-edit'>   
+    
+    <Card
+ hoverable
+ style={{ width: 250 }}
+ className='card-edit'
+ cover={<img alt="example" src={image} />}
+>
+<p className='rating-edit'>4.3 ★ | 40.9K</p>
+</Card>
+<br/>
+<h2>Data is here</h2>
+<p>Hello I am data</p>
+<p>Rs. 1499</p>
+</Col>
+  </Row>
+  <Row className='row-edit'>
+  <Col span={6} className='col-edit'>   
+    
+    <Card
+ hoverable
+ style={{ width: 250 }}
+ className='card-edit'
+ cover={<img alt="example" src={image} />}
+>
+<p className='rating-edit'>4.3 ★ | 40.9K</p>
+</Card>
+<br/>
+<h2>Data is here</h2>
+<p>Hello I am data</p>
+<p>Rs. 1499</p>
+</Col>
+  <Col span={6} className='col-edit'>   
+    
+    <Card
+ hoverable
+ style={{ width: 250 }}
+ className='card-edit'
+ cover={<img alt="example" src={image} />}
+>
+<p className='rating-edit'>4.3 ★ | 40.9K</p>
+</Card>
+<br/>
+<h2>Data is here</h2>
+<p>Hello I am data</p>
+<p>Rs. 1499</p>
+</Col>
+  <Col span={6} className='col-edit'>   
+    
+    <Card
+ hoverable
+ style={{ width: 250 }}
+ className='card-edit'
+ cover={<img alt="example" src={image} />}
+>
+<p className='rating-edit'>4.3 ★ | 40.9K</p>
+</Card>
+<br/>
+<h2>Data is here</h2>
+<p>Hello I am data</p>
+<p>Rs. 1499</p>
+</Col>
+  <Col span={6} className='col-edit'>   
+    
+    <Card
+ hoverable
+ style={{ width: 250 }}
+ className='card-edit'
+ cover={<img alt="example" src={image} />}
+>
+<p className='rating-edit'>4.3 ★ | 40.9K</p>
+</Card>
+<br/>
+<h2>Data is here</h2>
+<p>Hello I am data</p>
+<p>Rs. 1499</p>
+</Col>
+  </Row>
+  <Row className='row-edit'>
+  <Col span={6} className='col-edit'>   
+    
+    <Card
+ hoverable
+ style={{ width: 250 }}
+ className='card-edit'
+ cover={<img alt="example" src={image} />}
+>
+<p className='rating-edit'>4.3 ★ | 40.9K</p>
+</Card>
+<br/>
+<h2>Data is here</h2>
+<p>Hello I am data</p>
+<p>Rs. 1499</p>
+</Col>
+  <Col span={6} className='col-edit'>   
+    
+    <Card
+ hoverable
+ style={{ width: 250 }}
+ className='card-edit'
+ cover={<img alt="example" src={image} />}
+>
+<p className='rating-edit'>4.3 ★ | 40.9K</p>
+</Card>
+<br/>
+<h2>Data is here</h2>
+<p>Hello I am data</p>
+<p>Rs. 1499</p>
+</Col>
+  <Col span={6} className='col-edit'>   
+    
+    <Card
+ hoverable
+ style={{ width: 250 }}
+ className='card-edit'
+ cover={<img alt="example" src={image} />}
+>
+<p className='rating-edit'>4.3 ★ | 40.9K</p>
+</Card>
+<br/>
+<h2>Data is here</h2>
+<p>Hello I am data</p>
+<p>Rs. 1499</p>
+</Col>
+  <Col span={6} className='col-edit'>   
+    
+    <Card
+ hoverable
+ style={{ width: 250 }}
+ className='card-edit'
+ cover={<img alt="example" src={image} />}
+>
+<p className='rating-edit'>4.3 ★ | 40.9K</p>
+</Card>
+<br/>
+<h2>Data is here</h2>
+<p>Hello I am data</p>
+<p>Rs. 1499</p>
+</Col>
+  </Row>
+        </>    )
+} 
+
+export default Cards;
